cfe9475a93cb05237e8f223b5031a7aa768ddc3f
lisandroseia <lisandroseia11@gmail.com>
2022-01-12 09:57:33 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 import './style.css';
-import 'boxicons'
+import 'boxicons';
 
 const tasks = [
-    {
-        description: 'set webpack',
-        completed: false,
-        index:0
-    },
-    {
-        description: 'display items',
-        completed: false,
-        index:2
-    },
-    {
-        description: 'request review',
-        completed: false,
-        index:2
-    }
-]
+  {
+    description: 'set webpack',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'display items',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'request review',
+    completed: false,
+    index: 2,
+  },
+];
 
 const listWraper = document.querySelector('.li-wraper');
 
-for(let i = 0; i < tasks.length; i++){
-    const actualTask = document.createElement('li');
-    actualTask.className = 'task'
+for (let i = 0; i < tasks.length; i += 1) {
+  const actualTask = document.createElement('li');
+  actualTask.className = 'task';
 
-    actualTask.innerHTML = `
+  actualTask.innerHTML = `
     <div class="task-left">
     <input type="checkbox" name="${tasks[i].description}" id="${tasks[i].description}">
     <p>${tasks[i].description}</p></div>
 
-                        <box-icon name='dots-vertical-rounded'></box-icon>
-                          `
-    listWraper.appendChild(actualTask);
+                        <box-icon class="box-icon" name='dots-vertical-rounded'></box-icon>
+                          `;
+  listWraper.appendChild(actualTask);
 }
-
