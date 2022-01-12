601f87695da24b17280632a0aa5c6910d8377208
lisandroseia <lisandroseia11@gmail.com>
2022-01-12 09:41:45 -0300

    Set structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import './style.css';
+import 'boxicons'
 
-const title = document.querySelector('.title');
-console.log(title)
+const tasks = [
+    {
+        description: 'set webpack',
+        completed: false,
+        index:0
+    },
+    {
+        description: 'display items',
+        completed: false,
+        index:2
+    },
+    {
+        description: 'request review',
+        completed: false,
+        index:2
+    }
+]
+
+const listWraper = document.querySelector('.li-wraper');
+
+for(let i = 0; i < tasks.length; i++){
+    const actualTask = document.createElement('li');
+    actualTask.className = 'task'
+
+    actualTask.innerHTML = `
+    <div class="task-left">
+    <input type="checkbox" name="${tasks[i].description}" id="${tasks[i].description}">
+    <p>${tasks[i].description}</p></div>
+
+                        <box-icon name='dots-vertical-rounded'></box-icon>
+                          `
+    listWraper.appendChild(actualTask);
+}
 
