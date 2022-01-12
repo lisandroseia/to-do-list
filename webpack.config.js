36c2e9d19066dde6a8a6cf034980cf84a489d66f
lisandroseia <lisandroseia11@gmail.com>
2022-01-12 10:10:34 -0300

    Add files

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-    template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js', 
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
     static: './dist',
   },
